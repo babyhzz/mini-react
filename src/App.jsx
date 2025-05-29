706e6561383a075af75bb2c13f552ca477ed6070
hzz <383686357@qq.com>
2025-05-29 23:39:57 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,13 @@ import { useState } from "./react/react/React";
 export default function App() {
   const [count, setCount] = useState(1);
 
+  const handleClick = () => {
+    console.log('handleClick', count);
+    setCount(count => count + 1)
+  }
   return (
     <div>
-      <h1 onClick={() => setCount(count => count + 1)}>{count}</h1>
+      <h1 onClick={handleClick}>{count}</h1>
       <h2>aaaa</h2>
       <h1>1212121</h1>
     </div>
